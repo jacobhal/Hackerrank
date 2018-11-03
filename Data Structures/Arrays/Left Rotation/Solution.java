2a195fb345972c7d2ddd9fa5ba461aebabb67245
Jacob <jackeaik@hotmail.com>
2018-11-03 15:34:19 +0100

    Add: Repeated String

--- Data Structures/Arrays/Left Rotation/Solution.java
+++ Data Structures/Arrays/Left Rotation/Solution.java
@@ -11,46 +11,12 @@ public class Solution {
         int n = scan.nextInt();
         int d = scan.nextInt();
         int[] array = new int[n];
+        // Place each new element at previous index - rotation offset (need to add n to find the positive value)
         for(int i = 0; i < n; i++) {
             array[(i+n-d) % n] = scan.nextInt();
         }
         for(int i = 0; i < n; i++) {
             System.out.print(array[i] + " ");
-        }    
-
-/*
-        // Rotate a
-        a = rotate(d, a);
-        // Print result
-        print(a);
-        */
-    }
-
-    /*
-
-    public static int[] rotate(int nrOfRotations, int arr[]) {
-
-      while(nrOfRotations > 0) {
-        // First value of array
-        int firstValue = 0;
-        for(int i = 0; i < arr.length-1; i++) {
-          if(i == 0)
-            firstValue = arr[i];
-          arr[i] = arr[i+1];
         }
-        arr[arr.length-1] = firstValue;
-        nrOfRotations--;
-      }
-
-      return arr;
     }
-
-    public static void print(int arr[]) {
-      for(int i = 0; i < arr.length; i++) {
-        System.out.print(arr[i] + " ");
-      }
-      System.out.println();
-    }
-
-    */
 }
