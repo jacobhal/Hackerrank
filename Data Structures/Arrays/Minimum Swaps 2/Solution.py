fc94241b75308e48db12f8cd38542e7ac700456a
Jacob <jackeaik@hotmail.com>
2018-11-14 15:07:13 +0100

    Add: Minimum Swaps 2

---
+++ Data Structures/Arrays/Minimum Swaps 2/Solution.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def swap(arr, i1, i2):
+    tmp = arr[i1]
+    arr[i1] = arr[i2]
+    arr[i2] = tmp
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+
+    sortedArr = arr[:]
+    sortedArr.sort()
+    swapCounter = 0
+
+
+    while sortedArr != arr:
+        for i, e in enumerate(arr):
+            if e != sortedArr[i]:
+                swapIndex = e - 1
+                swap(arr, i, swapIndex)
+                swapCounter += 1
+
+    return swapCounter
+
+n = int(input())
+
+arr = list(map(int, input().rstrip().split()))
+
+res = minimumSwaps(arr)
+
+print(res)
